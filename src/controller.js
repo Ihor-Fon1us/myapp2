4017de7b5f29ed6cb584de2ecc50da0b1fa2a0af
fon1us <igor.rakitin.2000@gmail.com>
2022-04-12 11:14:10 +0300

    add validate

--- src/controller.js
+++ src/controller.js
@@ -4,14 +4,24 @@ const redirectToQuiz = () => {
 
 function redirectToEnd(form, event) {
   const formData = new FormData(form);
-  // for (var pair of formData.entries()) {
-  //   const data = (pair[0] + ': ' + pair[1]);
-  // }
+  
   event.preventDefault();
   const data = [...formData.values()]
   console.log(data[0].toString());
-  window.electronAPI.data("Name: " + data[0] + "\nSurname: " + data[1] + "\nDate of birth: " + data[2] + "\nEmail: " + data[3]);
-  window.location.href = "./end.html";
+  if (validateString(data[0].toString()) && validateString(data[1].toString()) && validateEmail(data[3].toString())) {
+    window.electronAPI.data("Name: " + data[0] + "\nSurname: " + data[1] + "\nDate of birth: " + data[2] + "\nEmail: " + data[3]);
+    window.location.href = "./end.html";
+  }
+}
+
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
+  }
+
+function validateString(name) {
+  var re = /^[a-zA-Z]+$/;
+  return re.test(name);
 }
 
 const turnExit = () => {
